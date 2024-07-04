cf8bb7281fb1ec586a74d1245e41ade1a3dff69a
Arnau <arnalvberita@gmail.com>
2024-07-04 13:43:15 +0200

    Excercici fet (per a corretgir)

--- src/main/java/n1exercici1/Main.java
+++ src/main/java/n1exercici1/Main.java
@@ -1,7 +1,60 @@
 package n1exercici1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args){
-        Undo.runProgram();
+        Undo undo = Undo.getInstance();
+        Scanner teclat = new Scanner(System.in);
+
+        boolean sortir = false;
+
+        do{
+            switch(menu(teclat)){
+                case 1:
+                    undo.afegirComanda(comanda(teclat));
+                    break;
+                case 2:
+                    undo.eliminarComanda();
+                    System.out.println("Última comanda desfeta!");
+                    break;
+                case 3:
+                    undo.llistarComandes();
+                    break;
+                case 4:
+                    System.out.println("Gràcies per utilitzar l'aplicació");
+                    sortir = true;
+                    break;
+            }
+        }while(!sortir);
+
+        teclat.close();
     }
+
+    public static byte menu(Scanner teclat){
+        byte opcio;
+        final byte MINIMO = 1;
+        final byte MAXIMO = 4;
+
+        do{
+            System.out.println("\nMENú PRINCIPAL");
+            System.out.println("1. Afegir comanda.");
+            System.out.println("2. Desfer última comanda.");
+            System.out.println("3. Llistar comandes.");
+            System.out.println("4. Sortir de l'aplicació.");
+            System.out.println("Selecciona una opció:");
+            opcio = teclat.nextByte();
+            teclat.nextLine();
+            if(opcio < MINIMO || opcio > MAXIMO){
+                System.out.println("Escull una opció vàlida");
+            }
+        }while(opcio < MINIMO || opcio > MAXIMO);
+        return opcio;
+    }
+
+    public static String comanda(Scanner teclat){
+        System.out.println("Introdueix la comanda: ");
+        return teclat.nextLine();
+    }
+
 }
--- src/main/java/n1exercici1/Undo.java
+++ src/main/java/n1exercici1/Undo.java
@@ -1,7 +1,42 @@
 package n1exercici1;
 
-public class Undo {
-    public static void runProgram(){
+import java.util.ArrayList;
+import java.util.List;
 
+public class Undo {
+
+    private static  Undo instancia;
+    private List<String> historialComandes;
+
+    private Undo(){
+        historialComandes = new ArrayList<>();
     }
+
+    public static Undo getInstance(){
+        if(instancia == null) {
+            instancia = new Undo();
+        }
+        return instancia;
+    }
+
+    public void afegirComanda (String command){
+        historialComandes.add(command);
+    }
+
+    public void eliminarComanda(){
+        if(!historialComandes.isEmpty()){
+            historialComandes.removeLast();
+        }
+    }
+
+    public void llistarComandes(){
+        if(historialComandes.isEmpty()){
+            System.out.println("No hi ha comandes a l'historial!");
+        } else {
+            for(int i = 0; i < historialComandes.size(); i++){
+                System.out.println((i + 1) + ".- " + historialComandes.get(i));
+            }
+        }
+    }
+
 }
